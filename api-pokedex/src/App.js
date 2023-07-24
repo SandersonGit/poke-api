767045d097d771fcc2999ac60fefc86d17b09897
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2023-07-24 20:13:31 -0300

    instalando as rotas com useNavigate

--- api-pokedex/src/App.js
+++ api-pokedex/src/App.js
@@ -1,30 +1,14 @@
 import { useState } from "react";
 import Header from "./Components/Header/Header";
-import PokemonListPage from "./Pages/PokemonListPage/PokemonListPage"
-import PokemonDetailPage from "./Pages/PokemonDetailPpage/PokemonDetailPage"
-import PokedexPage from "./Pages/PokedexPage/PokedexPage"
-
+import Router from "./Router/Router";
 
 function App() {
-  const [ page, setPage ] = useState(2);
-
+  
   return (
     <>
-      <Header page={page} setPage={setPage} />
+      {/* <Header page={page} setPage={setPage} /> */}
 
-      { page === 0 ? (
-        <PokemonListPage />
-
-      ) : page === 1 ? (
-        <PokedexPage />
-        
-      ) : (
-        <PokemonDetailPage />
-
-      )
-    
-    }
-
+      <Router />
 
     </>
   );
